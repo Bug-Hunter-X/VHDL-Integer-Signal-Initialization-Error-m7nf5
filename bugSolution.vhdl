2cc8faaa1f7f89781b0e93ad932f4ce642128b3e
BugHunter-X <askzenai@gmail.com>
2024-12-06 13:26:07 +0100

    Add bugSolution.vhdl file

---
+++ bugSolution.vhdl
@@ -0,0 +1,22 @@
+```vhdl
+entity counter is
+  Port ( clk : in  STD_LOGIC;
+              reset : in  STD_LOGIC;
+              count : out  INTEGER range 0 to 15); -- Integer range is specified here
+end entity;
+
+architecture behavioral of counter is
+  signal internal_count : INTEGER range 0 to 15 := 0; -- Correct initialization with =>
+begin
+  process (clk, reset)
+  begin
+    if reset = '1' then
+      internal_count <= 0;
+    elsif rising_edge(clk) then
+      internal_count <= internal_count + 1;
+    end if;
+  end process;
+
+  count <= internal_count;
+end architecture;
+```
